8565a16a1744838e30ba0c2b3a377840a10297f3
Brand_Git <brandgc1516@gmail.com>
2023-11-26 09:50:25 -0600

    Lista con diferentes colores

--- app/src/main/java/ucr/ac/cr/boxb/ui/home/ClientAdapter.java
+++ app/src/main/java/ucr/ac/cr/boxb/ui/home/ClientAdapter.java
@@ -3,15 +3,20 @@ package ucr.ac.cr.boxb.ui.home;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.core.content.ContextCompat;
+
 import java.util.ArrayList;
+import java.util.Random;
 
 import ucr.ac.cr.boxb.R;
 import ucr.ac.cr.boxb.model.Client;
@@ -50,9 +55,32 @@ public class ClientAdapter extends BaseAdapter {
         }
 
 
+        LinearLayout lL_background_adapter = view.findViewById(R.id.lL_background_adapter);
         TextView txt_cedula_adapter = view.findViewById(R.id.txt_cedula_adapter);
         TextView txt_name_adapter = view.findViewById(R.id.txt_name_adapter);
 
+//        Random random = new Random();
+//        int randomNumber = random.nextInt(3) + 1;
+
+        int modulo = position % 3;
+
+        int colorRes;
+        if (modulo == 0)
+        {
+            colorRes = R.color.soft_green;
+        }
+        else if (modulo == 1)
+        {
+            colorRes = R.color.soft_white;
+        } else {
+            colorRes = R.color.green;
+        }
+
+
+        int backgroundColor = ContextCompat.getColor(context, colorRes);
+        ColorDrawable colorDrawable = new ColorDrawable(backgroundColor);
+
+        lL_background_adapter.setBackground(colorDrawable);
         txt_name_adapter.setText(listClients.get(position).getName());
         txt_cedula_adapter.setText(listClients.get(position).getDocumentID());
 
